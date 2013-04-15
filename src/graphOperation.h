c4e5522987fd854cf5bd89bc3c0b1ef77f445736
ronaflx <900831flx@gmail.com>
2013-04-15 13:49:36 +0800

    write community vector

--- src/graphOperation.h
+++ src/graphOperation.h
@@ -17,10 +17,10 @@ using namespace std;
 void remove_edge(undirected_graph& g, int v, int u) {
 	auto i = find(g[v].begin(), g[v].end(), u);
 	auto j = find(g[u].begin(), g[u].end(), v);
-	if(i != g[v].end()) {
+	if (i != g[v].end()) {
 		g[v].erase(i);
 	}
-	if(j != g[u].end()) {
+	if (j != g[u].end()) {
 		g[u].erase(j);
 	}
 }
@@ -31,29 +31,31 @@ static void _add_edge(undirected_graph& g, int v, int u) {
 }
 
 void add_edge(undirected_graph& g, int v, int u) {
-	if(v >= int(g.size())) g.resize(v + 1);
-	if(u >= int(g.size())) g.resize(u + 1);
+	if (v >= int(g.size()))
+		g.resize(v + 1);
+	if (u >= int(g.size()))
+		g.resize(u + 1);
 	_add_edge(g, v, u);
 }
 void read_graph(undirected_graph& g, istream& in) {
 	int v, u;
-	while(in >> v >> u) {
+	while (in >> v >> u) {
 		add_edge(g, v, u);
 	}
 }
 
 void read_graph(undirected_graph& g, FILE* filp) {
 	int u, v;
-	while(fscanf(filp, "%d %d", &v, &u) == 2) {
+	while (fscanf(filp, "%d %d", &v, &u) == 2) {
 		add_edge(g, v, u);
 	}
 }
 
 void write_graph(const undirected_graph& g, ostream& out) {
 	int n = g.size();
-	for(int i = 0;i < n;i++) {
+	for (int i = 0; i < n; i++) {
 		out << i;
-		for(const auto & v : g[i]) {
+		for (const auto & v : g[i]) {
 			out << " " << v;
 		}
 		out << endl;
@@ -62,12 +64,29 @@ void write_graph(const undirected_graph& g, ostream& out) {
 
 void write_graph(const undirected_graph& g, FILE * filp) {
 	int n = g.size();
-	for(int i = 0;i < n;i++) {
+	for (int i = 0; i < n; i++) {
 		fprintf(filp, "%d", i);
-		for(const auto & v : g[i]) {
+		for (const auto & v : g[i]) {
 			fprintf(filp, " %d", v);
 		}
 		fprintf(filp, "\n");
+	}
+}
+void write_community(const communityV& cmtys, ostream& out) {
+	int n = cmtys.size();
+	for (int i = 0; i < n; i++) {
+		for (const auto& v : cmtys[i]) {
+			out << v << " " << i << endl;
+		}
+	}
+}
+
+void write_community(const communityV& cmtys, FILE* filp) {
+	int n = cmtys.size();
+	for (int i = 0; i < n; i++) {
+		for (const auto& v : cmtys[i]) {
+			fprintf(filp, "%d %d\n", v, i);
+		}
 	}
 }
 #endif /* GRAPHOPERATION_H_ */
