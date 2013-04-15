bbfaa9e4ccdb49007e6ac510a9d4a7a1a896e7d5
ronaflx <900831flx@gmail.com>
2013-04-15 12:37:58 +0800

    some graph operations

---
+++ src/graphOperation.h
@@ -0,0 +1,73 @@
+/*
+ * graphOperation.h
+ *
+ *  Created on: Apr 14, 2013
+ *      Author: ronaflx
+ */
+
+#ifndef GRAPHOPERATION_H_
+#define GRAPHOPERATION_H_
+#include "cmtyConfig.h"
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void remove_edge(undirected_graph& g, int v, int u) {
+	auto i = find(g[v].begin(), g[v].end(), u);
+	auto j = find(g[u].begin(), g[u].end(), v);
+	if(i != g[v].end()) {
+		g[v].erase(i);
+	}
+	if(j != g[u].end()) {
+		g[u].erase(j);
+	}
+}
+
+static void _add_edge(undirected_graph& g, int v, int u) {
+	g[v].push_back(u);
+	g[u].push_back(v);
+}
+
+void add_edge(undirected_graph& g, int v, int u) {
+	if(v >= int(g.size())) g.resize(v + 1);
+	if(u >= int(g.size())) g.resize(u + 1);
+	_add_edge(g, v, u);
+}
+void read_graph(undirected_graph& g, istream& in) {
+	int v, u;
+	while(in >> v >> u) {
+		add_edge(g, v, u);
+	}
+}
+
+void read_graph(undirected_graph& g, FILE* filp) {
+	int u, v;
+	while(fscanf(filp, "%d %d", &v, &u) == 2) {
+		add_edge(g, v, u);
+	}
+}
+
+void write_graph(const undirected_graph& g, ostream& out) {
+	int n = g.size();
+	for(int i = 0;i < n;i++) {
+		out << i;
+		for(const auto & v : g[i]) {
+			out << " " << v;
+		}
+		out << endl;
+	}
+}
+
+void write_graph(const undirected_graph& g, FILE * filp) {
+	int n = g.size();
+	for(int i = 0;i < n;i++) {
+		fprintf(filp, "%d", i);
+		for(const auto & v : g[i]) {
+			fprintf(filp, " %d", v);
+		}
+		fprintf(filp, "\n");
+	}
+}
+#endif /* GRAPHOPERATION_H_ */
